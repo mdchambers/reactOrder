d9c1de7199e107af3f264a1785873bbf47e6f2f0
Michael Chambers <mike@mdchambers.com>
2019-05-24 18:03:27 -0400

    Cleanup of old src

--- src_by_section/src_14_redux/store/actions/actionTypes.js
+++
@@ -1,9 +0,0 @@
-// Using action types
-// Reduces typs
-export const DECREMENT = 'DECREMENT';
-export const INCREMENT = 'INCREMENT';
-export const ADD = 'ADD';
-export const SUBTRACT = 'SUBTRACT';
-export const STORE = 'STORE';
-export const DELETE = 'DELETE';
-
